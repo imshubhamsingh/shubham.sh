91c17011671b041c4d27c8bcb884a825dd6386ce
Shubham Singh <imshubhamsingh97@gmail.com>
2021-09-14 00:45:53 +0530

    feat: update the current job

---
+++ components/CurrentWorkPlace/Smallcase.tsx
@@ -0,0 +1,14 @@
+function Smallcase() {
+  return (
+    <a
+      className="text-dark font-medium link"
+      target="_blank"
+      href="http://smallcase.com/about/?ref=shubham.sh"
+      rel="noreferrer"
+    >
+      <i className="icon-smallcase" /> smallcase
+    </a>
+  );
+}
+
+export default Smallcase;
